45180ca37cca63f2fce25f2c710442587b5a73de
Nilansh Bansal <bansalnilansh@gmail.com>
2018-03-07 16:52:40 +0530

    props passing

--- imports/startup/client/App.js
+++ imports/startup/client/App.js
@@ -4,7 +4,7 @@ import Header from "../../ui/Header";
 
 export const App=()=>{
     // return (<p className="pClass">this is a paragraph</p>)
-    return (<div><Header/><p className="pClass">this is a paragraph</p></div>)
+    return (<div><Header brandName="my Brand"/><p className="pClass">this is a paragraph</p></div>)
     
 }
 
--- imports/ui/Header.js
+++ imports/ui/Header.js
@@ -1,12 +1,12 @@
 import React from "react";
 
 
-export default Header=()=>{
+export default Header=(props)=>{
     // return (<p className="pClass">this is a paragraph</p>)
     return (<nav className="navbar navbar-default">
     <div className="container-fluid">
       <div className="navbar-header">
-        <a className="navbar-brand" href="#">WebSiteName</a>
+        <a className="navbar-brand" href="#">{props.brandName}</a>
       </div>
       <ul className="nav navbar-nav">
         <li className="active"><a href="#">Home</a></li>
