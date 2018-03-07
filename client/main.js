0744f43acef782e99baa24c07c83cebe14cd872b
Nilansh Bansal <bansalnilansh@gmail.com>
2018-03-07 15:12:26 +0530

    hands on at meteor

---
+++ client/main.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+export const App=()=>{
+    return (<p>this is a paragraph</p>)
+} 
+
+//argument that is function is passed to meteor 
+Meteor.startup(()=>{
+    ReactDOM.render(<App/>,document.querySelector(".render-target"));
+});
