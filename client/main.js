6ff319756bb4c7e8f9191bd3b5a6e7ad0ad41703
Nilansh Bansal <bansalnilansh@gmail.com>
2018-03-07 15:35:34 +0530

    styling added

--- client/main.js
+++ client/main.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 export const App=()=>{
-    return (<p>this is a paragraph</p>)
+    return (<p className="pClass">this is a paragraph</p>)
 } 
 
 //argument that is function is passed to meteor 
