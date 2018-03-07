add792f2d0ec5828c4c35dbda276b9a332028f09
Nilansh Bansal <bansalnilansh@gmail.com>
2018-03-07 17:22:33 +0530

    randomly changing brand name using faker

--- imports/startup/client/App.js
+++ imports/startup/client/App.js
@@ -1,10 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Header from "../../ui/Header";
+import faker from "faker";
+
+let brand={
+    name:""
+};
 
 export const App=()=>{
-    // return (<p className="pClass">this is a paragraph</p>)
-    return (<div><Header brandName="my Brand"/><p className="pClass">this is a paragraph</p></div>)
+    // let brand="VOCIQ";
+    const getBrands=()=>{
+        brand.name=faker.company.companyName();
+        console.log(brand.name);
+    }    
+    return (<div className="container"><Header brandName={brand.name}/><p className="pClass">this is a paragraph</p><button className="btn btn-primary btn-lg" onClick={getBrands}>Get Brand</button></div>)
     
 }
 
